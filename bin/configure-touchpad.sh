26978a81d38e69dc1381ec537524071b35d75319
Arthur McLain <mclain.it@gmail.com>
2023-04-15 06:25:14 +0200

    feat(bin): add touchpad configuration script

---
+++ bin/configure-touchpad.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############################################################################
+# Create Touchpad Config
+###############################################################################
+# Useful Info in /usr/share/X11/xorg.conf.d/40-libinput.conf
+
+MSG="Section \"InputClass\"\n\tIdentifier \"Touchpad\"\n\tMatchDriver \"libinput\"\n\tMatchIsTouchpad \"on\"\n\tOption \"Tapping\" \"on\"\n\tOption \"NaturalScrolling\" \"true\"\nEndSection"
+CONFIG="/etc/X11/xorg.conf.d/30-touchpad.conf"
+
+echo -e "$MSG" | sudo tee "$CONFIG" > /dev/null
